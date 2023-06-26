efb21d8feeaad1dc3a581360e9fc4dfd9ee9108f
Rodrigo Reis <rreisp@gmail.com>
2023-06-26 13:47:17 -0300

    Diving Into Generics

--- basics.ts
+++ basics.ts
@@ -31,28 +31,41 @@ let person: Person;
 
 person = {
   name: 'Max',
-  age: 32
+  age: 32,
 };
 
 // person = {
 //   isEmployee: true
 // };
 
-let people: {
-  name: string;
-  age: number;
-}[];
+let people: Person[];
 
 // Type inference
 
 let course: string | number = 'React - The Complete Guide';
 
 course = 12341;
+
 // Functions & types
+
 function add(a: number, b: number) {
-  return a + b
+  return a + b;
 }
 
 function print(value: any) {
   console.log(value);
 }
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd')
+
+// updatedArray[0].split('');
